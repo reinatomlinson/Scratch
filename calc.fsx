9c460c6d204a3fc155f8352f14429e768ab1ef05
Reina Tomlinson <reinatomlinson31@gmail.com>
2016-06-15 12:27:57 -0500

    latest

--- calc.fsx
+++ calc.fsx
@@ -33,10 +33,11 @@ let GetFileContent f = File.ReadAllLines(f)
 let ADRLines =
   for f in ListOfFileNames.[0..] do
     GetFileContent f |> Seq.filter(fun x -> if x.Contains("a-dark-room") then true else false)
+    |> Seq.toArray(fun x -> x) |> printfn "%A"
   //|> Seq.choose (fun x -> x.Split ' ') //|> printfn "%s" //(fun x -> x[2])//need to parse $ amts
 
-printfn ADRLines
-//ADRLines is type unit?????
+//printfn ADRLines
+//ADRLines is type unit == void basically
 
 //let ADRLinesSplit =
 //  Seq.toArray(ADRLines)// |> printfn "%A"
