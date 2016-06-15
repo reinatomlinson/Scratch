1c31bf236caa76e32962284f093e5e40d72a0ca9
Reina Tomlinson <reinatomlinson31@gmail.com>
2016-06-15 12:51:07 -0500

    some progress
    can get at each line now. still not splitting.

--- calc.fsx
+++ calc.fsx
@@ -33,7 +33,9 @@ let GetFileContent f = File.ReadAllLines(f)
 let ADRLines =
   for f in ListOfFileNames.[0..] do
     GetFileContent f |> Seq.filter(fun x -> if x.Contains("a-dark-room") then true else false)
-    |> Seq.toArray(fun x -> x) |> printfn "%A"
+    |> (fun x -> Seq.toArray(x))
+    |> (fun line -> for x in line.[0..] do printfn "%s" x)//x.Split)
+    //|> printfn "%A"
   //|> Seq.choose (fun x -> x.Split ' ') //|> printfn "%s" //(fun x -> x[2])//need to parse $ amts
 
 //printfn ADRLines
